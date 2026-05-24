c9c58f168a80da9533ab8136c12bf786467cd4f0
Vinicius Assis <vini1904@hotmail.com>
2026-05-24 11:37:54 -0300

    ..

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const { analisarSintatico } = require('./sintatico');
 
 console.log("Iniciando Compilação...\n");
 
-// 1. Roda o Léxico obtendo os tokens ou o erro léxico
 const resultadoLexico = analisarLexico('entrada.txt');
 
 if (!resultadoLexico.sucesso) {
     console.log("❌ FALHA NA COMPILAÇÃO (Fase Léxica):");
     console.log(resultadoLexico.erro);
 } else {
-    // 2. Passa a lista de tokens direto para o Sintático na memória
     const resultadoSintatico = analisarSintatico(resultadoLexico.tokens);
     
     if (resultadoSintatico.status === "SUCESSO") {
